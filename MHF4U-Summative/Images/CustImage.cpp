8b507d04eebc2f51b61fa2f3badab1e2fa048b37
Benjamin Wilkins <wilkben@yahoo.ca>
2017-01-07 16:45:36 -0500

    removed useless files

--- MHF4U-Summative/Images/CustImage.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  CustImage.cpp
-//  MHF4U-Summative
-//
-//  Created by Benjamin Wilkins on -01-0617.
-//  Copyright © 2017 Benjamin Wilkins. All rights reserved.
-//
-
-#include "CustImage.hpp"
